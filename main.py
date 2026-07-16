b4fe8c332bca19fd9bf261182b555cf156b6052b
Shravani-Chaudhari-394 <shravanihc394@gmail.com>
2026-07-16 23:02:40 +0530

    Fix syntax error in main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://sahayak-frontend-phi.vercel.app/"
+        "https://sahayak-frontend-phi.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
